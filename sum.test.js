cd43ffb574935388a27e121c1584f6b77d4b0fbe
Braden Soller <braden.soller18@gmail.com>
2025-10-01 11:00:02 -0500

    more practice with tesing, toEqual, toBe, and Falsy

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,20 @@
 const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
+    //toBe() works best for numbers, boleans, and strings.
     expect(sum(1,2)).toBe(3);
+});
+
+test('object assignment', () => { 
+    //creating an object named data with a property named one with the value of 1.
+    const data = { one: 1 }
+    //adds a proprety named two with the value of 2 to the data object.
+    data['two'] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+});
+
+
+test('null is falsy', () => {
+    const n = null;
+    //this checks if anwsers are undefined or null.
+    expect(n).toBeFalsy();
 });
