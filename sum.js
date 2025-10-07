9841835c29ed2d812b48cc6b32b3a6da9df328b1
Braden Soller <braden.soller18@gmail.com>
2025-10-07 12:16:14 -0500

    testing anon functions with callbacks

--- sum.js
+++ sum.js
@@ -1,16 +1,22 @@
-function sum(a,b){
-    return a + b;
+//function sum(a,b){
+//    return a + b;
+//}
+
+
+
+
+//function myFunction(input) {
+ //   if (typeof input !== 'number') {
+ //       throw new Error("Invalid input");
+ // }
+//}
+
+function fetchData(callback) { 
+    setTimeout(() => {
+        callback("peanut butter");
+    }, 1000)
+     
 }
 
-
-
-module.exports = sum;
-
-function myFunction(input) {
-    if (typeof input !== 'number') {
-        throw new Error("Invalid input");
-  }
-
-}
     
-module.exports = myFunction;
+module.exports = fetchData;
--- sum.test.js
+++ sum.test.js
@@ -1,34 +1,16 @@
-const sum = require('./sum');
-const myFunction = require('./sum');
-test('adds 1 + 2 to equal 3', () => {
-    //toBe() works best for numbers, boleans, and strings.
-    expect(sum(1,2)).toBe(3);
-});
-
-test('object assignment', () => { 
-    //creating an object named data with a property named one with the value of 1.
-    const data = { one: 1 }
-    //adds a proprety named two with the value of 2 to the data object.
-    data['two'] = 2;
-    expect(data).toEqual({ one: 1, two: 2 });
-});
+const fetchData = require("./sum");
 
 
-test('null is falsy', () => {
-    const n = null;
-    //this checks if anwsers are undefined, null, zero, or false 
-    expect(n).toBeFalsy();
-});
-
-test('null is falsy', () => {
-    const n = null;
-    //this checks if anwsers are undefined, null, zero, or false 
-    expect(n).toBeFalsy();
-});
-
-test("throws on invalid input ", () => {
-    expect(() => {
-      //error throwing function 
-    myFunction("fdgfg");
-  }).toThrow();
+//done is parameter that performs a callback to let jest know when the test is complete.
+test("the data is peanut butter", done => {
+    function callback(data) {
+        try {
+            expect(data).toBe("peanut butter");
+            //this is where done is called to let jest know the test is complete.
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+    fetchData(callback);
 });
